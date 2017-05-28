844f6d01dfbf148d5f12279bb5c475c05903e269
bill <tuntunkimpo@naver.com>
2017-05-28 23:42:30 +0900

    예제 파일의 객체와 배열을 통해 rand함수를 통한 난수 발생으로 새로고침했을때 항상 새로운 문제가 나올수 있도록 함 >> 1가지문제만 나오도록 하고 타이머를 장착해야함

--- portfolio/exercise.js
+++ portfolio/exercise.js
@@ -180,7 +180,279 @@ $(document).click(function(loc) {
 });
 
 
+//jQuery
 
+Lesson 1:
+The Basics: the DOM, $, and Selectors
+ 1. Course Introduction
+ 2. Why Does jQuery Exist?
+ 3. Quiz: What is jQuery actually
+ 4. Money, Money, Money (the $)
+ 5. How to Use the $
+ 6. Quiz: DOM Inspiration
+ 7. How Is jQuery Included in a Page?
+ 8. Quiz: Select by Tags
+ 9. Quiz: Select by Classes Quiz
+ 10. Quiz: Select by IDs
+ 11. Family Tree Revisited
+ 12. Quiz: Filters Quiz
+ 13. High Five!
+
+//Lesson1
+// index.html
+<!DOCTYPE html>
+<!--
+
+This is the HTML document that you'll use jQuery to modify. To take the quiz, click over to app.js!
+
+-->
+<html lang="en">
+<head>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <div class="container">
+        <header class="header">
+            <nav id="nav" class="navigator">
+                <h1>Nav Header</h1>
+                <ul class="nav-list">
+                    <li class="nav-item"><a>Item #1</a></li>
+                    <li class="nav-item active"><a href="#2">Item #2</a></li>
+                    <li class="nav-item"><a href="#3">Item #3</a></li>
+                    <li class="nav-item"><a href="#4">Item #4</a></li>
+                    <li class="nav-item"><a href="#5">Item #5</a></li>
+                    <li class="nav-item"><a href="#6">Item #6</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="articles">
+            <input id="input" value="Cool Articles" type="text">
+            <h1>Cool Articles</h1>
+            <ul class="article-list">
+                <li class="article-item">
+                    <header>Article #1</header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita sapiente officiis beatae, ut consequuntur. Quos minus neque eius, nemo sunt excepturi eveniet amet veritatis voluptatibus corporis ea, blanditiis porro ad!</p>
+                    <h3>Sample Image Title here</h3>
+                    <img src="http://placehold.it/350x150" alt="Placeholder Image">
+                    <ul>
+                        <li class="bold">James</li>
+                        <li>Lily</li>
+                        <li>Harry</li>
+                    </ul>
+                    <p>Magnam ex autem doloremque, tempore mollitia atque aut delectus corporis rem similique voluptates omnis reiciendis vitae impedit exercitationem unde quaerat, doloribus voluptatibus molestias et veritatis sed optio repudiandae? Provident, voluptates.</p>
+                </li>
+                <li class="article-item featured">
+                    <header>Article #2</header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil animi ipsum, incidunt mollitia modi cum, eum ex doloremque rerum quod, maiores quisquam, enim quam unde cumque! Quam, doloremque. Voluptatum, maxime!</p>
+                    <p>Numquam et quae, quasi. Reiciendis nemo mollitia eveniet alias, debitis facere atque excepturi et beatae laudantium commodi optio unde amet vitae libero quas cupiditate, nam porro minus. Quisquam, odit non.</p>
+                </li>
+                <li class="article-item">
+                    <header>Article #3</header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ratione, cum doloremque molestias labore amet at dicta reiciendis repudiandae fuga atque adipisci pariatur distinctio suscipit perferendis provident, facere ad aut.</p>
+                    <p>Temporibus sapiente odio eveniet enim perferendis maxime, ratione, accusantium suscipit alias, soluta architecto culpa pariatur. Cupiditate nam eaque dolore voluptatem necessitatibus corporis iusto adipisci, dignissimos error, vitae quam, aliquid eius!</p>
+                </li>
+                <li class="article-item">
+                    <header>Article #4</header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate commodi, est soluta eos voluptates eum! Modi sit quia ipsum fugiat nesciunt, assumenda deleniti consequuntur molestias reprehenderit. Voluptate corporis illum nam?</p>
+                    <p>Quia, omnis. Quasi necessitatibus, ducimus, deserunt ipsa, reprehenderit sit a dolore fuga placeat magni culpa ipsam quas id aut veniam rerum pariatur enim harum recusandae quo odio consequuntur. Maxime, aperiam.</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
+// app.js
+// selecting by class name
+/*
+For this quiz, use a jQuery class selector to grab all of the elements of class 'article-item' on the page!
+*/
+// don't change this variable!
+var articleItems;
+
+articleItems = $('.article-item');// your code goes here!
+console.log(articleItems);
+     
+// selecting by id name
+/*
+For this quiz, use a jQuery class selector to grab all the element with id 'nav' on the page!
+*/
+
+// don't change this variable!
+var nav;
+
+nav = $('nav');// your code goes here!
+console.log(nav);
+                    
+
+                    
+                    
+//Lesson1-12 filter quiz
+//index.html
+<!DOCTYPE html>
+<!--
+
+This is the HTML document that you'll use jQuery to modify. To take the quiz, click over to app.js!
+
+-->
+<html lang="en">
+<head>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <div class="container">
+        <header class="header">
+            <nav id="nav" class="navigator">
+                <h1>Nav Header</h1>
+                <ul class="nav-list">
+                    <li class="nav-item"><a>Item #1</a></li>
+                    <li class="nav-item active"><a href="#2">Item #2</a></li>
+                    <li class="nav-item"><a href="#3">Item #3</a></li>
+                    <li class="nav-item"><a href="#4">Item #4</a></li>
+                    <li class="nav-item"><a href="#5">Item #5</a></li>
+                    <li class="nav-item"><a href="#6">Item #6</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="articles">
+            <input id="input" value="Cool Articles" type="text">
+            <h1>Cool Articles</h1>
+            <ul class="article-list">
+                <li class="article-item">
+                    <header>Article #1</header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita sapiente officiis beatae, ut consequuntur. Quos minus neque eius, nemo sunt excepturi eveniet amet veritatis voluptatibus corporis ea, blanditiis porro ad!</p>
+                    <h3>Sample Image Title here</h3>
+                    <img src="http://placehold.it/350x150" alt="Placeholder Image">
+                    <ul>
+                        <li class="bold">James</li>
+                        <li>Lily</li>
+                        <li>Harry</li>
+                    </ul>
+                    <p>Magnam ex autem doloremque, tempore mollitia atque aut delectus corporis rem similique voluptates omnis reiciendis vitae impedit exercitationem unde quaerat, doloribus voluptatibus molestias et veritatis sed optio repudiandae? Provident, voluptates.</p>
+                </li>
+                <li class="article-item featured">
+                    <header>Article #2</header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil animi ipsum, incidunt mollitia modi cum, eum ex doloremque rerum quod, maiores quisquam, enim quam unde cumque! Quam, doloremque. Voluptatum, maxime!</p>
+                    <p>Numquam et quae, quasi. Reiciendis nemo mollitia eveniet alias, debitis facere atque excepturi et beatae laudantium commodi optio unde amet vitae libero quas cupiditate, nam porro minus. Quisquam, odit non.</p>
+                </li>
+                <li class="article-item">
+                    <header>Article #3</header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ratione, cum doloremque molestias labore amet at dicta reiciendis repudiandae fuga atque adipisci pariatur distinctio suscipit perferendis provident, facere ad aut.</p>
+                    <p>Temporibus sapiente odio eveniet enim perferendis maxime, ratione, accusantium suscipit alias, soluta architecto culpa pariatur. Cupiditate nam eaque dolore voluptatem necessitatibus corporis iusto adipisci, dignissimos error, vitae quam, aliquid eius!</p>
+                </li>
+                <li class="article-item">
+                    <header>Article #4</header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate commodi, est soluta eos voluptates eum! Modi sit quia ipsum fugiat nesciunt, assumenda deleniti consequuntur molestias reprehenderit. Voluptate corporis illum nam?</p>
+                    <p>Quia, omnis. Quasi necessitatibus, ducimus, deserunt ipsa, reprehenderit sit a dolore fuga placeat magni culpa ipsam quas id aut veniam rerum pariatur enim harum recusandae quo odio consequuntur. Maxime, aperiam.</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
+                                 
+//app.js
+/*
+For this quiz, use articleList and DOM navigation methods to collect articleList's
+sibling <h1> (var h1), children (var kids), and parent <div>s (var parents).
+
+You must use articleList to navigate to the element(s)!
+*/
+
+// Start with these variable!
+var articleList, h1, kids, parents;
+
+articleList = $('.article-list');
+
+h1 = articleList.siblings(h1);// Your code goes here!
+
+kids = articleList.find('*');// Your code goes here!
+
+parents = articleList.parents('div');// Your code goes here!
+
+console.log(h1, kids, parents);
+
+                    
+//Lesson2-2 toggleClass
+//index.html
+<!DOCTYPE html>
+<!--
+
+This is the HTML document that you'll use jQuery to modify. To take the quiz, click over to app.js!
+
+-->
+<html lang="en">
+<head>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <div class="container">
+        <header class="header">
+            <nav id="nav" class="navigator">
+                <h1>Nav Header</h1>
+                <ul class="nav-list">
+                    <li class="nav-item"><a>Item #1</a></li>
+                    <li class="nav-item active"><a href="#2">Item #2</a></li>
+                    <li class="nav-item"><a href="#3">Item #3</a></li>
+                    <li class="nav-item"><a href="#4">Item #4</a></li>
+                    <li class="nav-item"><a href="#5">Item #5</a></li>
+                    <li class="nav-item"><a href="#6">Item #6</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div class="articles">
+            <input id="input" value="Cool Articles" type="text">
+            <h1>Cool Articles</h1>
+            <ul class="article-list">
+                <li class="article-item">
+                    <header>Article #1</header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita sapiente officiis beatae, ut consequuntur. Quos minus neque eius, nemo sunt excepturi eveniet amet veritatis voluptatibus corporis ea, blanditiis porro ad!</p>
+                    <h3>Sample Image Title here</h3>
+                    <img src="http://placehold.it/350x150" alt="Placeholder Image">
+                    <ul>
+                        <li class="bold">James</li>
+                        <li>Lily</li>
+                        <li>Harry</li>
+                    </ul>
+                    <p>Magnam ex autem doloremque, tempore mollitia atque aut delectus corporis rem similique voluptates omnis reiciendis vitae impedit exercitationem unde quaerat, doloribus voluptatibus molestias et veritatis sed optio repudiandae? Provident, voluptates.</p>
+                </li>
+                <li class="article-item featured">
+                    <header>Article #2</header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil animi ipsum, incidunt mollitia modi cum, eum ex doloremque rerum quod, maiores quisquam, enim quam unde cumque! Quam, doloremque. Voluptatum, maxime!</p>
+                    <p>Numquam et quae, quasi. Reiciendis nemo mollitia eveniet alias, debitis facere atque excepturi et beatae laudantium commodi optio unde amet vitae libero quas cupiditate, nam porro minus. Quisquam, odit non.</p>
+                </li>
+                <li class="article-item">
+                    <header>Article #3</header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ratione, cum doloremque molestias labore amet at dicta reiciendis repudiandae fuga atque adipisci pariatur distinctio suscipit perferendis provident, facere ad aut.</p>
+                    <p>Temporibus sapiente odio eveniet enim perferendis maxime, ratione, accusantium suscipit alias, soluta architecto culpa pariatur. Cupiditate nam eaque dolore voluptatem necessitatibus corporis iusto adipisci, dignissimos error, vitae quam, aliquid eius!</p>
+                </li>
+                <li class="article-item">
+                    <header>Article #4</header>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate commodi, est soluta eos voluptates eum! Modi sit quia ipsum fugiat nesciunt, assumenda deleniti consequuntur molestias reprehenderit. Voluptate corporis illum nam?</p>
+                    <p>Quia, omnis. Quasi necessitatibus, ducimus, deserunt ipsa, reprehenderit sit a dolore fuga placeat magni culpa ipsam quas id aut veniam rerum pariatur enim harum recusandae quo odio consequuntur. Maxime, aperiam.</p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
+                    
+                    
+//app.js
+/*
+For this quiz, use a jQuery class selector and featuredArticle variable to toggle the 'featured' class!
+*/
+
+// don't change this variable!
+var featuredArticle;
+
+featuredArticle = $('.featured');
+
+featuredArticle.toggleClass('featured');
+
+                    
 
 
 
